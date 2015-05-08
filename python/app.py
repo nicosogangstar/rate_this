b5f28191774c58cd384c44a26899e7cfee3e260b
Burning Pixel <burningpixel123@gmail.com>
2015-05-08 17:20:01 -0400

    Updated query handling

--- python/app.py
+++ python/app.py
@@ -9,8 +9,17 @@ def make_json_list(arg_list):
         template.format(i) for i in arg_list
     )
     return "'" + list_without_quotes + "'"
-
 # print(make_json_list([1,2,3,4]))
+
+
+class QueryText:
+    pass
+
+QueryText.popular = 'POPULAR'
+QueryText.recent  = 'RECENT'
+QueryText.month   = 'MONTH'
+QueryText.day     = 'DAY'
+QueryText.year    = 'YEAR'
 
 
 @app.route('/')
@@ -20,9 +29,15 @@ def home():
 
 @app.route('/entries/<query_type>/<time_frame>')
 def entries(query_type=None, time_frame=None):
+
     # query_type = popular or recent
     # time_frame = day, month, year, all time
-    return "Time Frame"
+
+    if query_type == QueryText.popular:
+        return 'POP'
+
+    if query_type == QueryText.recent:
+        return 'RECENT'
 
 
 
