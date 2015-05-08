a2498e6ad9ee5bef71af47bb9891c2ae6841c8ac
Burning Pixel <burningpixel123@gmail.com>
2015-05-08 16:55:07 -0400

    Updated app.py

--- python/app.py
+++ python/app.py
@@ -1,8 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 
+def make_json_list(arg_list):
+    template = '[{}],'
+    list_without_quotes = ''.join(
+        template.format(i) for i in arg_list
+    )
+    return "'" + list_without_quotes + "'"
+
+# print(make_json_list([1,2,3,4]))
+
+
 @app.route('/')
 def home():
-    return
+    return "Home Page"
+
+
+@app.route('/popular/<time_frame>')
+def popular(time_frame=None):
+    return "Time Frame"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
