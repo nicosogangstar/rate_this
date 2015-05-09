c34bef5d4891aebd34b985d059a0dd303b310001
Nicolas Gonzalez <nicolasnicogonzalez@gmail.com>
2015-05-09 03:12:37 +0000

    updated dbreader to work as a function

--- python/dbreader.py
+++ python/dbreader.py
@@ -1,20 +1,30 @@
 import pymysql
 
-con = pymysql.connect(host='localhost', user='root', passwd='iloveyeeting', db='ratemybitches')
+def fetch_data(strcolumn = 'id', start=1, end=1):
+	con = pymysql.connect(host='localhost', user='root', passwd='iloveyeeting', db='ratemybitches')
+	start-=1
+	result = ''
+	if strcolumn=='id':
+		column = 0
 
-start = 3
-start-=1
-end = 5
-column = 3
+	elif strcolumn=='extention':
+		column = 1
 
+	elif strcolumn=='timestamp':
+		column = 2
 
-with con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM img LIMIT " + str(start) + "," + str(end))
-    
-    rows = cur.fetchmany(size=(end-start))
-    for row in rows:
-    	print(row[column])
+	elif strcolumn=='upvotes':
+		column = 3
 
-cur.close()
-con.close()
+	with con:
+	    cur = con.cursor()
+	    cur.execute("SELECT * FROM img LIMIT " + str(start) + "," + str(end))
+	    
+	    rows = cur.fetchmany(size=(end-start))
+	    for row in rows:
+	    	result = result + str(row[column]) + ", "
+	cur.close()
+	con.close()
+	return result[0:-2]
+
+print(fetch_data('id', 1, 2))
