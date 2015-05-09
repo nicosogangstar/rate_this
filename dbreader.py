86de5b13d1f3407c70ba041d3dcbcc371a0a5a6c
System Administrator <root@Greg.local>
2015-05-09 09:58:45 -0400

    made dbreader fully functional

---
+++ dbreader.py
@@ -0,0 +1,40 @@
+import pymysql
+
+def fetch_data(strcolumn = 'id', start=1, end=1):
+	con = pymysql.connect(host='localhost', user='root', passwd='iloveyeeting', db='ratemybitches')
+	result = ''
+	if strcolumn=='id':
+
+		start-=1
+		with con:
+		    cur = con.cursor()
+		    cur.execute("SELECT id, extension FROM img LIMIT " + str(start) + "," + str(end))
+		    
+		    rows = cur.fetchmany(size=(end-start))
+		    for row in rows:
+		    	result = result + str(row[0]) + row[1] + ", "
+
+	elif strcolumn=='timestamp':
+
+		with con:
+		    cur = con.cursor()
+		    cur.execute("SELECT id, extension FROM img WHERE time BETWEEN \'" + start + "\' AND \'" + end + "\'")
+		    rows = (cur.fetchall())
+		    for row in rows:
+		    	result = result + str(row[0]) + row[1] + ", "
+
+	elif strcolumn=='upvotes':
+
+		with con:
+		    cur = con.cursor()
+		    cur.execute("SELECT id, extension FROM img WHERE upvotes BETWEEN " + str(start) + " AND " + str(end))
+		    rows = (cur.fetchall())
+		    for row in rows:
+		    	result = result + str(row[0]) + row[1] + ", "
+	cur.close()
+	con.close()
+	return result[0:-2]
+
+print(fetch_data('timestamp', '2000-03-26 00:00:01', '2003-03-26 00:00:01'))
+print(fetch_data('id', 3, 5))
+print(fetch_data('upvotes', 200, 500))
--- dbsetup.py
+++ dbsetup.py
@@ -8,11 +8,14 @@ try:
         cur = con.cursor()
         cur.execute("DROP DATABASE IF EXISTS ratemybitches")
         cur.execute("CREATE DATABASE ratemybitches")
-        cur.execute("USE ratemybitches")
-        # timestamp format: ss:mm:hh:dd:mm:yyyy
-        cur.execute("CREATE TABLE img (id int(3) NOT NULL AUTO_INCREMENT, file_extension varchar(5) NOT NULL, time varchar(19) NOT NULL, upvotes int(5) NOT NULL, KEY(id))") 
+        # timestamp format: yyyy-mm-dd hh-mm-ss
+        cur.execute("CREATE TABLE ratemybitches.img (id int(3) NOT NULL AUTO_INCREMENT, extension varchar(5) NOT NULL, time TIMESTAMP DEFAULT 0 NOT NULL, upvotes int(5) NOT NULL, KEY(id))") 
         # insert test data for interation with other applications/files
-        cur.execute("INSERT INTO ratemybitches.img (id, file_extension, time, upvotes) VALUES (1, '.jpg', '60:60:24:31:12:2000', 100)")
+        cur.execute("INSERT INTO ratemybitches.img (id, extension, time, upvotes) VALUES (1, '.jpg', '2001-01-05 00:00:01', 100)")
+        cur.execute("INSERT INTO ratemybitches.img (id, extension, time, upvotes) VALUES (2, '.gif', '2002-02-04 00:00:01', 200)")
+        cur.execute("INSERT INTO ratemybitches.img (id, extension, time, upvotes) VALUES (3, '.png', '2003-03-03 00:00:01', 300)")
+        cur.execute("INSERT INTO ratemybitches.img (id, extension, time, upvotes) VALUES (4, '.tif', '2004-04-02 00:00:01', 400)")
+        cur.execute("INSERT INTO ratemybitches.img (id, extension, time, upvotes) VALUES (5, '.lol', '2005-05-01 00:00:01', 500)")
 
     # result = con.use_result()
     # print "MySQL version: %s" % \
