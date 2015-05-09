4eff6dc97a36ab143734523fb238b6ec425267a4
Nicolas Gonzalez <nicolasnicogonzalez@gmail.com>
2015-05-09 00:12:38 +0000

    remodeled file system

---
+++ static/top.js
@@ -0,0 +1,38 @@
+function get_popular(timeFrame, start, end){
+	$.ajax({
+		"method":"POST",
+		"url":"popular",
+		"data": {
+			"time":timeFrame,
+			"start":start,
+			"end":end
+		},
+
+		//return handlers
+		"success":function( response ){
+			console.log(response);
+		},
+		"error":function(){
+			console.log("Kek u ded D:");
+		}
+	});
+}
+
+function get_newest(start, end){
+	$.ajax({
+		method:"POST",
+		url:"newest",
+		data: {
+			"start":start,
+			"end":end
+		},
+
+		//return handlers
+		success:function( response ){
+			console.log(response);
+		},
+		error:function(){
+			console.log("Kek u ded D:");
+		}		
+	});
+}
